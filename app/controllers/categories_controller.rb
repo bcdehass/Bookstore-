61b9a7e17ca6b49cba27dcb95d9f5236faf8de3c
bcdehass <bcdehass@gmail.com>
2017-07-24 13:33:58 -0400

    added validation to categories with success message

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,9 +6,13 @@ class CategoriesController < ApplicationController
 
   def create
     @category = Category.new(category_params) #create new category using the params from the private function at the bottom of this file
-    @category.save
-    
-    redirect_to categories_path 
+    if @category.save #if the save is successful, redirect. if it's not, reload the create form
+      flash[:notice] = "Category Created" #flashes message. must update the layout page for it to work on all pages
+
+      redirect_to categories_path 
+    else 
+      render 'new'
+    end 
 
   end
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ActiveRecord::Base
 	has_many :books #one category can have many books
+
+	#validation. presence means if it's required or not, length is min length of the string in this case
+	validates :name, presence: true, length: {minimum:3}
 end
